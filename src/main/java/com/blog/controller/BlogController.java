a7aa1d7eb4a299216d2a6a3b938a2ad70b71246a
pvenugop <pravinvenugopal47@gmail.com>
2018-02-08 00:11:28 -0500

    Initial APIs

--- src/main/java/com/blog/controller/BlogController.java
+++ src/main/java/com/blog/controller/BlogController.java
@@ -3,15 +3,12 @@ package com.blog.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
 
-@RestController
-@RequestMapping(value = "/")
+@Controller
 public class BlogController {
 
-    @RequestMapping(value = "/welcome", method = RequestMethod.GET)
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String welcome() {
 //        model.put("message", "HelloWorld");
         return "welcome";
